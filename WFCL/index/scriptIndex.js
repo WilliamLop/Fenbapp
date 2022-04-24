eba6d85d4711de61a6ab2ff2bae7bf7520b52bac
William <calderonwilliam25@gmail.com>
2022-04-23 19:24:36 -0500

    Parte de index terminada, falta animaciones js

---
+++ WFCL/index/scriptIndex.js
@@ -0,0 +1,25 @@
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
+}
+
+function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
+}
+
+function openCity(evt, cityName) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("city");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < x.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" w3-border-green", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.firstElementChild.className += " w3-border-green";
+  }
+
+w3.slideshow(".slides", 2500);
